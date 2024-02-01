5d23c3ce86704214603a390e83430d19926abddd
tkirk4 <157310015+tkirk4@users.noreply.github.com>
2024-02-01 09:57:45 -0600

    Add files via upload

---
+++ rectangle.py
@@ -0,0 +1,12 @@
+"""
+File: rectangle.py
+Project 1.4
+Computes and prints the area of a rectangle, given an
+input width and height.
+"""
+
+width = int(input("Enter the width: "))
+height = int(input("Enter the height: "))
+area = width * height
+print("The area is", area, "square units.")
+
